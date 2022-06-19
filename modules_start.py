f0820d4e45cbef4219ea8924f0d393bffcd98c71
Amit Kumar <amit.kumar@biz2credit.com>
2022-06-19 10:06:00 +0530

    modules

---
+++ modules_start.py
@@ -0,0 +1,7 @@
+# TODO: import the math module, which containe features for working with math
+import math
+# TODO: the math module contains lots of pre-built functions
+print("the square root of 16 is", math.sqrt(16 ))
+# TODO: in additio to  functions, some modules conytain useful constants
+print("Pi is ", math.pi)
+# TODO: try some of the math function for yourself here:
